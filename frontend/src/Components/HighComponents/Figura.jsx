f2618a36ac916a2a0040dd8979a93f89929d9298
brhenri-mr <superbr55@gmail.com>
2023-05-29 23:38:38 -0300

    comm041
    Implementação de textos da quantidade de carregamento nos carregamentos distribuidos, melhorias na segmentação dos carregamentos

--- frontend/src/Components/HighComponents/Figura.jsx
+++ frontend/src/Components/HighComponents/Figura.jsx
@@ -7,16 +7,77 @@ import Viga from "../SVG/Viga";
 //Constante
 import PadraoParaDesenho from "../../Constants/PadraoParaDesenho";
 
+
+
+function carregamentodistribuidounico(carregamentos){
+    //carregamentos: objeto contendo os carregamentos
+    let saida = []
+    const carregamentoFiltrado = []
+    const pontuais = []
+    const elementos = []
+    const posicoes = []
+    let tempMag = 0
+
+    //filtrando por objjetos de tipo distruibuido
+    for(let chave of Object.keys(carregamentos)){
+        if (carregamentos[chave]['tipo'] === "Distribuido"){
+            carregamentoFiltrado.push(carregamentos[chave])
+            elementos.push(carregamentos[chave]['pos'][0])
+            elementos.push(carregamentos[chave]['pos'][1])  
+        }
+        else{
+            pontuais.push(carregamentos[chave])
+            elementos.push(carregamentos[chave]['pos'][0])
+            
+        }
+    }
+  
+    //posicoes sem ser repedido
+    let elementosfiltrados = elementos.filter(function(este, i) {
+    return elementos.indexOf(este) === i;});
+    //Organizando a lista
+    elementosfiltrados.sort(function(a, b){return a-b})
+
+    
+    for(let i =0;i<=(elementosfiltrados.length-2);i++){
+        posicoes.push([elementosfiltrados[i],elementosfiltrados[i+1]])
+    }
+
+
+    for(let trecho of posicoes){
+        for(let valores of carregamentoFiltrado){
+            
+            if(valores['pos'][0]<=trecho[0] && valores['pos'][1]>=trecho[1]){
+                tempMag = tempMag + valores['mag']
+            }
+        }
+        saida.push({'tipo':'Distribuido','mag':tempMag,'pos':trecho})
+        tempMag = 0
+    }
+    console.log(saida)
+
+
+
+
+
+    return saida
+}
+
+
+
 const Figura = (props) =>{
 
     let magmax = 0
+
+    const CarregamentoParaDesenho = carregamentodistribuidounico(props.carregamentos)
+
+
 
     for(let item of props.carregamentos){
         if (magmax>=item['mag']){
             magmax = item['mag']
         }
     }
-
     return (
         <div>
             <svg {...PadraoParaDesenho}>
@@ -24,11 +85,12 @@ const Figura = (props) =>{
                 {props.apoios.map((item,index)=>{
                     return <Apoio key= {index} tipo = {item.tipo} value={item.value}></Apoio>
                 })}
-                {Object.values(props.carregamentos).map((item,index) =>{
+                {CarregamentoParaDesenho.map((item,index) =>{
                     if(item['tipo'] ==='Pontual'){
                         return <CarregamentoPontual key={index} mag={item['mag']} start={item['pos'][0]+50}></CarregamentoPontual>
                     }
                     else if(item['tipo']==='Distribuido'){
+
                         return <CarregamentoDist key={index} comprimento={item['pos'][1]-item['pos'][0]} mag={item['mag']} start={item['pos'][0]+50}></CarregamentoDist>
                     }
                 })}
--- frontend/src/Components/SVG/CarregamentosDist.jsx
+++ frontend/src/Components/SVG/CarregamentosDist.jsx
@@ -24,7 +24,7 @@ const CarregamentoDist = (props)=>{
     }
 
     return (<>
-
+        <text x={props.start+(props.comprimento-3*12)/2} y={147.5-props.mag-5} fontSize={12}>{`${props.mag} kN/m`}</text>
         <rect x={props.start} y={cotainicial-props.mag} width={props.comprimento} height={props.mag} className="cargaDistribuidaFundo"></rect>
         <line x1={props.start} y1={cotainicial-props.mag} x2={comprimentoreal} y2={cotainicial-props.mag} className="cargaDistribuida"></line>
         {quantidade_arrow.map((item,index)=>{return <Arrow key ={index} x1={item} y1={147} x2={item} y2={147-props.mag} mag={props.mag}></Arrow> })}
