e7f4b68c329e6e01ff69b7c32b8b3bbfc9340bba
brhenri-mr <superbr55@gmail.com>
2023-06-15 17:43:09 -0300

    comm076
    Correção dos eixos

--- frontend/src/Components/SVG/Eixos.jsx
+++ frontend/src/Components/SVG/Eixos.jsx
@@ -3,18 +3,18 @@ import React from "react";
 
 const Eixos= (props) =>{
 
-    const flechasBaixo = `${50},62 ${50+3},59 ${50},67 ${50-3},59 ${50},62`
+    const flechasBaixo = `${50},32 ${50+3},29 ${50},37 ${50-3},29 ${50},32`
 
 
 
     return (
         <>
-            <line x1={50} y1={30} x2={50} y2={62} className="cargaDistribuida"></line>
+            <line x1={50} y1={3} x2={50} y2={32} className="cargaDistribuida"></line>
             <polygon points={flechasBaixo} className="cargaDistribuida"></polygon>
-            <text x="47" y="85">y</text>
-            <line x1={50} y1={30} x2={82} y2={30} className="cargaDistribuida"></line>
-            <polygon points={`${82},30 ${82-3},33 ${82+5},30 ${82-3},27 ${82},30`} className="cargaDistribuida"></polygon>
-            <text  x="95" y="35">x</text>
+            <text x="47" y="50">y</text>
+            <line x1={50} y1={3} x2={82} y2={3} className="cargaDistribuida"></line>
+            <polygon points={`${82},3 ${82-3},6 ${82+5},3 ${82-3},0 ${82},3`} className="cargaDistribuida"></polygon>
+            <text  x="95" y="12">x</text>
         </>
     )
 }
