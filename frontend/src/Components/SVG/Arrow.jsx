b1260f61c6b24d380d9f2c32bcc29f613e517dc7
brhenri-mr <superbr55@gmail.com>
2023-06-22 14:01:03 -0300

    comm088
    Correção de posição do carregamento distribuído negativo

--- frontend/src/Components/SVG/Arrow.jsx
+++ frontend/src/Components/SVG/Arrow.jsx
@@ -2,12 +2,12 @@ import React from "react";
 
 
 const Arrow = (props) => {
-    const flechasCima  =`${props.x1},152 ${props.x1+3},157 ${props.x1},149 ${props.x1-3},157 ${props.x1},152`
+    const flechasCima  =`${props.x1},155 ${props.x1+3},160 ${props.x1},152 ${props.x1-3},160 ${props.x1},155`
     const flechasBaixo = `${props.x1},142 ${props.x1+3},139 ${props.x1},147 ${props.x1-3},139 ${props.x1},142`
 
     const utilizar = (parseFloat(props.mag)>0)?flechasBaixo:flechasCima
 
-    return (
+    return (    
         <>
             <line x1={props.x1} y1={props.y1} x2={props.x2} y2={props.y2} className="cargaDistribuida"></line>
             <polygon points={utilizar} className="cargaDistribuida"></polygon>
